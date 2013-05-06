6aa0b337185c5638f5e9b838053389cb86a0edc8
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-05-06 20:37:17 +0300

    Release 0.6

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ long_description = (
 
 setup(
     name='collective.pfg.payment',
-    version='0.5',
+    version='0.6',
     description="Extends PloneFormGen to payment form.",
     long_description=long_description,
     classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7"],
